1ef7ddda198fd679729d735e86eade6024b6a34f
RabeDatta <robedutt@gmail.com>
2022-11-06 14:47:44 -0500

    completed the server side

---
+++ server/routes/authRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { signup, login } = require("../controller/authControllers.js");
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+module.exports = router;
